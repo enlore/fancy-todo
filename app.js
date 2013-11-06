2008b26925f701f1c13d3fa576ebef099df9a27c
enlore <n.e.lorenson@gmail.com>
2013-11-06 01:02:39 -0600

    Fahr it up!

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express')
+    , app = express()
+    , less_middleware = require('less-middleware')
+    , path = require('path')
+    , port = 9023
+
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(less_middleware({
+    src: path.join(__dirname, 'static'),
+    compress: false,
+    debug: true,
+    prefix: 'css',
+    paths: [path.join(__dirname, 'static')]
+}))
+
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
+app.listen(port)
+console.log('** Listening on ' + port)
